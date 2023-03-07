e8c11c60f92351cc115dcea67391795ad04ef5e7
niteeshchaudhary <nkchaudhary00@gmail.com>
2023-03-07 12:19:30 +0530

    Permission2

--- app/src/main/java/com/example/smarttravels/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/smarttravels/Fragments/HomeFragment.java
@@ -4,12 +4,15 @@ package com.example.smarttravels.Fragments;
 import static com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
@@ -17,6 +20,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.smarttravels.MainActivity;
 import com.example.smarttravels.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -34,6 +38,7 @@ public class HomeFragment extends Fragment {
     Button b1;
     TextView latitude;
     TextView longitude;
+    public static final int PERMISSION_GOT = 7;
 
     public HomeFragment(Context myContext) {
         this.myContext = myContext;
@@ -63,24 +68,43 @@ public class HomeFragment extends Fragment {
                     if (ActivityCompat.checkSelfPermission(myContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(myContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                         // TODO: Consider calling
                         //    ActivityCompat#requestPermissions
+                        requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
                         // here to request the missing permissions, and then overriding
                         // public void onRequestPermissionsResult(int requestCode, String[] permissions,int[] grantResults)
                         // to handle the case where the user grants the permission. See the documentation
                         // for ActivityCompat#requestPermissions for more details.
+
+                        // other 'case' lines to check for other
+                        // permissions this app might request
+
+
                         return;
+                    }else {
+                        Task<Location> locationTask = fusedLocationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, null)
+                                .addOnSuccessListener((OnSuccessListener<Location>) location -> {
+                                    // Got last known location. In some rare situations this can be null.
+                                    if (location != null) {
+                                        latitude.setText("Latitude :" + location.getLatitude());
+                                        longitude.setText("Longitude :" + location.getLongitude());
+                                    }
+                                });
                     }
-                    Task<Location> locationTask = fusedLocationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY,null)
-                            .addOnSuccessListener((OnSuccessListener<Location>) location -> {
-                                // Got last known location. In some rare situations this can be null.
-                                if (location != null) {
-                                    latitude.setText("Latitude :"+location.getLatitude());
-                                    longitude.setText("Longitude :"+location.getLongitude());
-                                }
-                            });
-            }
-        });
+                }
+            });
         }
     }
-
-
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    ActivityCompat.requestPermissions((Activity) myContext, new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 1);// Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            });
 }
+
--- app/src/main/java/com/example/smarttravels/MainActivity.java
+++ app/src/main/java/com/example/smarttravels/MainActivity.java
@@ -1,77 +1,68 @@
 package com.example.smarttravels;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.smarttravels.Adapters.MyStateAdapter;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-//    TabLayout tabLayout;
+    //    TabLayout tabLayout;
 //    ViewPager viewPager;
-        TabLayout tabLayout;
-        ViewPager2 viewPager;
+    public static final int PERMISSION_GOT = 6;
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tabLayout=(TabLayout)findViewById(R.id.tab_layout);
-        viewPager=(ViewPager2)findViewById(R.id.view_pager);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        viewPager = (ViewPager2) findViewById(R.id.view_pager);
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
 
-//        tabLayout.addTab(tabLayout.newTab().setText("Home"));
-//        tabLayout.addTab(tabLayout.newTab().setText("Sport"));
-//        tabLayout.addTab(tabLayout.newTab().setText("Movie"));
+            MyStateAdapter adapter = new MyStateAdapter(this, getSupportFragmentManager(), getLifecycle(), 3);
+            viewPager.setAdapter(adapter);
 
-        MyStateAdapter adapter = new MyStateAdapter(this,getSupportFragmentManager(), getLifecycle(),3);
-        viewPager.setAdapter(adapter);
+            new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+                // set the text for each tab
+                switch (position) {
+                    case 0:
+                        tab.setText("Home");
+                        break;
+                    case 1:
+                        tab.setText("Avail");
+                        break;
+                    case 2:
+                        tab.setText("Options");
+                        break;
+                }
+            }).attach();
+        }
 
-        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
-            // set the text for each tab
-            switch (position) {
-                case 0:
-                    tab.setText("Home");
-                    break;
-                case 1:
-                    tab.setText("Avail");
-                    break;
-                case 2:
-                    tab.setText("Options");
-                    break;
-            }
-        }).attach();
-    }
+        private ActivityResultLauncher<String> requestPermissionLauncher =
+                registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                    if (isGranted) {
+                        // Permission is granted. Continue the action or workflow in your
+                        // app.
+                    } else {
+                        this.requestPermissions( new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, PERMISSION_GOT);
+                        // Explain to the user that the feature is unavailable because the
+                        // feature requires a permission that the user has denied. At the
+                        // same time, respect the user's decision. Don't link to system
+                        // settings in an effort to convince the user to change their
+                        // decision.
+                    }
+                });
 
-//        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
-//        viewPager=(ViewPager)findViewById(R.id.viewPager);
-//
-//        tabLayout.addTab(tabLayout.newTab().setText("Home"));
-//        tabLayout.addTab(tabLayout.newTab().setText("Sport"));
-//        tabLayout.addTab(tabLayout.newTab().setText("Movie"));
-//        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-//
-//        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(), tabLayout.getTabCount());
-//        viewPager.setAdapter(adapter);
-//
-//        viewPager.addOnPageChangeListener( new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-//
-//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {
-//                viewPager.setCurrentItem(tab.getPosition());
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {
-//
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//
-//            }
-//        });
-
- //   }
 }
